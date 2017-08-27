ac3168e0a3174465eb0e1293875b75c3c12e8505
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2017-08-28 02:27:50 +0530

    added vcs.xml

--- src/com/janbask/training3/main/HibernateJpaEx.java
+++ src/com/janbask/training3/main/HibernateJpaEx.java
@@ -8,6 +8,7 @@ import org.hibernate.SessionFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import java.util.Date;
 
 public class HibernateJpaEx {
